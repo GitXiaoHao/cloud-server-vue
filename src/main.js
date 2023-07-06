515175317474ae23876fdff2015a0a194764606d
yu <2486245007@qq.com>
2023-07-06 22:11:42 +0800

    rabbit静态资源

--- src/main.js
+++ src/main.js
@@ -3,17 +3,15 @@ import App from './App.vue'
 
 import {loadPlugins} from "@/plugins/index.js";
 import router from "@/router/Index.js";
-
+//引入rabbit初始化样式
+import '@/rabbit/styles/common.scss'
 // import './style.css'
 const app = createApp(App)
 /** 加载插件 */
 loadPlugins(app)
 /** 添加路由 */
 
-//TODO 测试接口函数
-/*import {getHitokoto} from "@/rabbit/apis/testApi.js";
-let hitokoto = getHitokoto();
-console.log(hitokoto)*/
+
 app.use(router)
 router.isReady().then(() => {
     app.mount("#app")
